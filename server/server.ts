332634ee537dddbf97975e62be758fb4f86993bd
James Lovejoy <james.lovejoy2@gmail.com>
2021-02-27 09:33:04 -0600

    added lodash lodash types

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import * as _ from 'lodash';
 
 const port: string | number = process.env.port || 1337;
 
 const app = express();
-
+  
 app.listen(port);
 app.use(express.static("public"))
 
