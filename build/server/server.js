6e36fadc32997aecf3a7a07131ab0e5adc2fe596
James Lovejoy <james.lovejoy2@gmail.com>
2021-03-01 06:43:44 -0600

    built new client and server js

--- build/server/server.js
+++ build/server/server.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var path_1 = __importDefault(require("path"));
 var questions = [{ title: 'How to log in?', content: "How do I log in?", answerCount: 2 }];
 var port = process.env.port || 1337;
 var app = express_1.default();
@@ -12,5 +13,9 @@ app.use(express_1.default.static("public"));
 console.log("Listening on port: " + port);
 app.get("/questions", function (_req, res) {
     res.json(questions);
+});
+app.get("/main.js", function (_req, res) {
+    //serves main.js file to the client
+    res.sendFile(path_1.default.resolve(__dirname, "..", "client", "client.js"));
 });
 //# sourceMappingURL=server.js.map
